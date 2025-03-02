7265db02fb2807a4ebcd24d43d58e23bd91fb40f
Uladzimir Swenum <wetoster@gmail.com>
2025-03-02 12:14:15 +0000

    Add  description work with SSH and github

---
+++ hw1/top_7_directories.sh
@@ -0,0 +1,9 @@
+﻿#!/usr/bin/bash
+
+output_file="top_directories.txt"
+
+# Получаем топ-7 директорий, занимающих больше всего места
+du -ah / | sort -rh | head -n 7 > "$output_file"
+
+
+echo "Топ-7 директорий, занимающих больше всего места, записан в файл $output_file"
---
+++ hw2/hw2-3.sh
@@ -0,0 +1,15 @@
+﻿#!/bin/bash
+# Запрос имени пользователя
+read -p "Введите имя пользователя: " username
+# Проверка существования пользователя
+if id "$username" &>/dev/null; then
+    echo "Пользователь '$username' уже существует."
+else
+    # Создание пользователя
+    sudo useradd -m "$username"
+    if [ $? -eq 0 ]; then
+        echo "Пользователь '$username' успешно создан."
+    else
+        echo "Ошибка при создании пользователя '$username'."
+    fi
+fi
